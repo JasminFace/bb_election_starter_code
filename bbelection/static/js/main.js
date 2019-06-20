fdc59fc6e9e536a72713e0d01e22ab7b6d329a72
Jasmin Saromo <jasminsaromo@gmail.com>
2019-06-20 16:21:00 -0400

    made get response for candidates

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+    axios.get('https://bb-election-api.herokuapp.com/')
+        .then(response => {
+            let data = response.data.candidates;
+            var list = document.getElementById('candidates');
+            data.forEach(candidate => {
+                let liTag = document.createElement('li');
+                liTag.innerText = candidate.name + "\n" + "Votes: " + candidate.votes
+                list.appendChild(liTag)
+            });
+        });
 });
